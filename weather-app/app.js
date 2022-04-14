0203b781db377b90a29e3b0b8c61345ca8b238ab
Thanh Phan <thanh.phan@halliburton.com>
2022-04-14 22:27:06 +0700

    33. An HTTP Request Challenge (n3-06-06-geocode)

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,3 +5,14 @@ const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452
 request({ url: url, json: true }, (error, response) => {
     console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.")
 })
+
+// Geocoding
+// Address -> Lat/Long -> Weather
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+    const latitude = response.body.features[0].center[0]
+    const longitude = response.body.features[0].center[1]
+    console.log(latitude, longitude)
+})
