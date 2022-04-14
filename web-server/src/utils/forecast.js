d4797edd8c4193d1ab049af0327a1b2c62bd2701
Thanh Phan <thanh.phan@halliburton.com>
2022-04-14 23:23:58 +0700

    68. New Feature Deployment Workflow (with-updates-n3-09-09-new-feature)

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -1,15 +1,15 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=' + latitude + ',' + longitude + '&units=f'
 
-    request({ url, json: true }, (error, { body }) => {
+    request({ url: url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degress out. The humidity is " + body.current.humidity + "%.")
         }
     })
 }
