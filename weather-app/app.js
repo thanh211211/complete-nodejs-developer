73e40b6359420119889832ce3293a6d9a4e93b66
Thanh Phan <thanh.phan@halliburton.com>
2022-04-14 22:35:54 +0700

    37. Callback Abstraction Challenge (with-updates-n3-06-10-callback-abstraction-2)

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,35 +1,12 @@
-const request = require('request')
 const geocode = require('./utils/geocode')
-
-// const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=42.3605,-71.0596&units=f'
-
-// request({ url: url, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to weather service!')
-//     } else if (response.body.error) {
-//         console.log('Unable to find location')
-//     } else {
-//         console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.")
-//     }
-// })
-
- //const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/philadelphia.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
-
- //request({ url: geocodeURL, json: true }, (error, response) => {
- //    if (error) {
- //        console.log('Unable to connect to location services!')
- //    } else if (response.body.features.length === 0) {
- //        console.log('Unable to find location. Try another search.')
- //    } else {
- //        const latitude = response.body.features[0].center[0]
- //        const longitude = response.body.features[0].center[1]
- //        console.log(latitude, longitude)
- //    }
- //})
-
-
+const forecast = require('./utils/forecast')
 
 geocode('Boston', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+})
+
+forecast(44.1545, -75.7088, (error, data) => {
     console.log('Error', error)
     console.log('Data', data)
 })
